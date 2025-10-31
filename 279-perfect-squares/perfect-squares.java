1ecc171e86bd06eab1eec4e0ebacbbb0c42767be
Koushikjha <koushik70422@gmail.com>
2025-10-31 10:42:14 +0530

    Time: 37 ms (46.22%) | Memory: 42.6 MB (86.43%) - LeetSync

--- 279-perfect-squares/perfect-squares.java
+++ 279-perfect-squares/perfect-squares.java
@@ -1,10 +1,16 @@
 class Solution {
     public int numSquares(int n) {
-        int[] memo=new int[n+1];
-        Arrays.fill(memo,-1);
-        int ans=find(n,memo);
-        return ans;
+        int[] dp=new int[n+1]; //DP
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        dp[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                dp[i]=Math.min(dp[i],1+dp[i-(j*j)]);
+            }
+        }
+        return dp[n];
     }
+    //Recursion
     int find(int n, int[] memo){
         if(n==0){
             return 0;
