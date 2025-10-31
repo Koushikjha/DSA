5fc0640fd37828f1580a4419cb499bb77cf664d9
Koushikjha <koushik70422@gmail.com>
2025-10-31 15:59:31 +0530

    Time: 70 ms (5.41%) | Memory: 43.5 MB (6.27%) - LeetSync

--- 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
+++ 2031-egg-drop-with-2-eggs-and-n-floors/egg-drop-with-2-eggs-and-n-floors.java
@@ -1,6 +1,24 @@
 class Solution {
     public int twoEggDrop(int n) {
-        return (int) Math.ceil((Math.sqrt(1 + 8.0 * n) - 1) / 2);
+        int[][] dp=new int[n+1][3];
+        for(int i=0;i<=n;i++){
+            Arrays.fill(dp[i],-1);
+        }
+        return find(n,2,dp);
+    }
+    public int find(int f,int e,int[][] dp){
+        if(f==0 || f==1 || e==1){
+            return f;
+        }
+        if(dp[f][e]!=-1){
+            return dp[f][e];
+        }
+        int ans=Integer.MAX_VALUE;
+        for(int i=1;i<=f;i++){
+            int s=1+Math.max(find(i-1,e-1,dp),find(f-i,e,dp));
+            ans=Math.min(s,ans);
+        }
+        return dp[f][e]=ans;
 
     }
 }
