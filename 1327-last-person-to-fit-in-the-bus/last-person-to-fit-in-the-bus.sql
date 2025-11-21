f5b5e0d447bf4ae5f66a99adf6258441d846784f
Koushik Jha <koushik70422@gmail.com>
2025-11-21 13:51:12 +0530

    Time: 817 ms (46.49%) | Memory: 0B (100.00%) - LeetSync

--- 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
+++ 1327-last-person-to-fit-in-the-bus/last-person-to-fit-in-the-bus.sql
@@ -1,13 +1,6 @@
-with ste as
-(
-    with cte as(
+with cte as(
     select 
     *,
     sum(weight) over (order by turn) as s
     from Queue
-)
-select *,
-row_number() over(order by turn desc) as rn
-from cte where s<=1000
-)
-select person_name from ste where rn=1;
+)select person_name from cte where s<=1000 order by turn desc limit 1;
