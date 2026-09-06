694935507ee3056efccadd1ba38b90ec497291f1
Koushik Jha <koushik70422@gmail.com>
2026-09-06 12:41:17 +0530

    Time: 106 ms (5.20%) | Memory: 60.8 MB (8.84%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -16,10 +16,10 @@ class Solution {
         int n=t.length();
         x=t.length();
         memo=new Integer[n+1][s.length()];
-        return find(s,"",n,"",0);
+        return find(s,"",n,0);
         
     }
-    public int find(String s,String p,int n,String mask,int i){
+    public int find(String s,String p,int n,int i){
         if(n==0){
             return 1;
         }
@@ -33,9 +33,9 @@ class Solution {
         int a=0;
         int b=0;
         if(c==t.charAt(x-n) ){
-            a=find(s,p+c,n-1,mask+" "+i,i+1);
+            a=find(s,p+c,n-1,i+1);
         }
-        b=find(s,p,n,mask,i+1);
+        b=find(s,p,n,i+1);
         return memo[n][i]=a+b;
     }
 }
