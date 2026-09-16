18ced05f934f6e4671f6fdf1afd7056853eff289
Koushik Jha <koushik70422@gmail.com>
2026-09-16 19:05:14 +0530

    Time: 193 ms (5.11%) | Memory: 47.1 MB (14.20%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -9,40 +9,46 @@
  * }
  */
 class Solution {
-    public ListNode mergeKLists(ListNode[] list) {
-        if(list.length==0){
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists.length==0){
             return null;
         }
-        for(int i=0;i<list.length-1;i++){
-            ListNode node=merge(list[i],list[i+1]);
-            list[i+1]=node;
+        PriorityQueue<ListNode> pq=new PriorityQueue<>((a,b)->Integer.compare(a.val,b.val));
+        for(ListNode head : lists){
+            if(head!=null){
+                pq.offer(head);
+            }
         }
-        return list[list.length-1];
+        while(pq.size()>1){
+            ListNode a=pq.poll();
+            ListNode b=pq.poll();
+            ListNode head=merge(a,b);
+            pq.offer(head); 
+        }
+        return pq.poll();
     }
     public ListNode merge(ListNode a,ListNode b){
         ListNode dummy=new ListNode(-1);
         ListNode curr=dummy;
-        ListNode x=a;
-        ListNode y=b;
-        while(x!=null && y!=null){
-            if(x.val<y.val){
-                curr.next=x;
-                x=x.next;
+        while(a!=null && b!=null){
+            if(a.val<b.val){
+                curr.next=a;
+                a=a.next;
             }else{
-                curr.next=y;
-                y=y.next;
+                curr.next=b;
+                b=b.next;
             }
             curr=curr.next;
         }
-        while(x!=null){
-            curr.next=x;
-            x=x.next;
+        while(a!=null){
+            curr.next=a;
             curr=curr.next;
+            a=a.next;
         }
-        while(y!=null){
-            curr.next=y;
-            y=y.next;
+        while(b!=null){
+            curr.next=b;
             curr=curr.next;
+            b=b.next;
         }
         return dummy.next;
     }
