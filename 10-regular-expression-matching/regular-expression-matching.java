dc8080e3289928c94ad73574628f6e3983c95f6a
Koushik Jha <koushik70422@gmail.com>
2026-09-21 17:25:15 +0530

    Time: 2 ms (45.61%) | Memory: 43.5 MB (63.20%) - LeetSync

--- 10-regular-expression-matching/regular-expression-matching.java
+++ 10-regular-expression-matching/regular-expression-matching.java
@@ -9,20 +9,18 @@ class Solution {
         }
         for(int i=1;i<=s.length();i++){
             for(int j=1;j<=p.length();j++){
-                if(s.charAt(i-1)==p.charAt(j-1) || p.charAt(j-1)=='.'){
+                if(p.charAt(j-1)==s.charAt(i-1) || p.charAt(j-1)=='.'){
                     dp[i][j]=dp[i-1][j-1];
                 }else if(p.charAt(j-1)=='*'){
                     dp[i][j]=dp[i][j-2];
                     char prev=p.charAt(j-2);
-                    if(prev==s.charAt(i-1) || prev=='.'){
-                        dp[i][j]=dp[i][j] || dp[i-1][j];
+                    if(s.charAt(i-1)==prev || prev=='.'){
+                        dp[i][j]=dp[i][j]||dp[i-1][j];
                     }
-                }else{
-                    dp[i][j]=false;
                 }
             }
+            
         }
-        
         return dp[s.length()][p.length()];
     }
 }
